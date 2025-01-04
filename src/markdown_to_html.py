5f706c5756fc5e6969d2a207a8d020377d46d492
JacobA12 <jacob.adams121202@gmail.com>
2025-01-04 12:02:37 -0800

    Implement markdown block processing in markdown_to_html_blocks function

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,2 +1,14 @@
+from markdown_blocks import markdown_to_blocks, block_to_block_type
+from htmlnode import HTMLNode
+
 def markdown_to_html_blocks(markdown):
+  #Step 1: split the markdown into blocks
+  filtered_blocks = markdown_to_blocks(markdown)
   
+  #Step 2: For Each Block
+  #Determine the block type 
+  for block in filtered_blocks:
+    type = block_to_block_type(block)
+    #Create an HTML Node for that block type
+    new_htmlNode = HTMLNode(type, block)
+    
