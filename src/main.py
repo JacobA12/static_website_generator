492acc17955ba56c9cd8835fdbb8e1c114cd45bf
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2025-01-04 19:44:25 -0800

    Add static_to_public function for copying files from static to public directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from textnode import TextNode, TextType
+from static_to_public import static_to_public
+import os
 
 
 def main():
+    current_directory = "static"
+    destination = "public"
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
+
+    static_to_public(current_directory, destination)
 
 
 main()
