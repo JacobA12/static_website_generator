a1397aed34b3d4e3eba867d2bcf06543235648c5
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2025-01-04 20:47:19 -0800

    Add initial content, HTML template, and title extraction functionality

---
+++ src/generate_page.py
@@ -0,0 +1,9 @@
+from markdown_blocks import *
+
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line.lstrip("#").strip()
+    raise Exception("No Title Found")
---
+++ src/test_generate_page.py
@@ -0,0 +1,25 @@
+from generate_page import *
+import unittest
+
+
+class TestGeneratePage(unittest.TestCase):
+    def test_extract_title(self):
+        md = "# Hello"
+        expected = "Hello"
+
+        self.assertEqual(extract_title(md), expected)
+
+    def test_extract_title_exception(self):
+        md = """This is not a tittle
+      This is also not a title
+      #Close but not a title"""
+        with self.assertRaises(Exception) as context:
+            extract_title(md)
+        self.assertEqual(str(context.exception), "No Title Found")
+
+    def test_extract_title_2(self):
+        md = """This is not a title
+#This is close to a title
+# This is the title"""
+        expected = "This is the title"
+        self.assertEqual(extract_title(md), expected)
