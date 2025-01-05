2912959b04669c6e86c0e06c801b0b0640a904f7
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2025-01-04 21:17:27 -0800

    Enhance page generation: integrate title extraction, improve HTML output, and add test cases for title extraction functionality

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,28 +1,34 @@
-from htmlnode import *
-from markdown_blocks import *
-
-
-def extract_title(markdown):
-    lines = markdown.split("\n")
-    for line in lines:
-        if line.startswith("# "):
-            return line.lstrip("#").strip()
-    raise Exception("No Title Found")
+import os
+from markdown_blocks import markdown_to_html_node
 
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page {from_path} to {dest_path} using {template_path}")
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
 
-    with open(from_path, "r") as f:
-        markdown = f.read()
-    html = markdown_to_html_node(markdown).to_html()
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
 
-    with open(template_path, "r") as f:
-        template = f.read()
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
 
-    with open(dest_path, "w") as f:
-        f.write(
-            template.replace("{{content}}", html).replace(
-                "{{title}}", extract_title(markdown)
-            )
-        )
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
